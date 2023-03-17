f44e701a08a14f32e1c7c950dc39feb9bda691e2
ikerovec20 <ikerovec20@foi.hr>
2023-03-17 12:44:33 +0100

    Add project files.

---
+++ DanasNET914/EventHandlers.cs
@@ -0,0 +1,45 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.Handlers;
+using Exiled.Events.EventArgs.;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.Events.EventArgs.Scp914;
+using MapGeneration.Distributors;
+using InventorySystem.Items.Pickups;
+using Exiled.API.Features.Items;
+
+namespace DanasNET914
+{
+    public class EventHandlers
+    {
+        private Plugin plugin;
+
+        public EventHandlers(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnItemUpgrade(UpgradingInventoryItemEventArgs ev)
+        {
+            if (ev.Item.Type == ItemType.Radio && ev.KnobSetting == Scp914.Scp914KnobSetting.Coarse && plugin.rng.Next(100) <= 50)
+            {
+                ev.IsAllowed = false;
+                ev.Item.Destroy();
+                ev.Player.AddItem(ItemType.SCP1576);
+            }
+        }
+
+        public void OnPickupUpgrade(UpgradingPickupEventArgs ev)
+        {
+            if (ev.Pickup.Type == ItemType.Radio && ev.KnobSetting == Scp914.Scp914KnobSetting.Coarse && plugin.rng.Next(100) <= 50)
+            {
+                ev.IsAllowed = false;
+                ev.Pickup.Destroy();
+                ev.
+            }
+        }
+    }
+}
---
+++ DanasNET914/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using Exiled.API.Features;
+using Exiled.Loader;
+using InventorySystem.Disarming;
+
+namespace DanasNET914
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "Keroshka";
+        public override string Name { get; } = "DanasDotNET914";
+        public override Version Version { get; } = new Version(6,0,0);
+
+        public static Plugin Instance = new Plugin();
+
+        public EventHandlers EventHandler { get; private set; }
+
+        public Random rng = new Random();
+
+        public override void OnEnabled()
+        {
+            EventHandler = new EventHandlers(Instance);
+
+            Exiled.Events.Handlers.Scp914.OnUpgradingInventoryItem += EventHandler.OnItemUpgrade;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Scp914.OnUpgradingInventoryItem -= EventHandler.OnItemUpgrade;
+            base.OnDisabled();
+        }
+    }
+}
